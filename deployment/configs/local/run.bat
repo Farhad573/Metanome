4c66c601a9eff785244a6b1ddd2d94d8c93cc551
Farhad573 <farhademami13@gmail.com>
2026-04-09 14:40:35 +0200

    all changes required for deployment

--- deployment/configs/local/run.bat
+++ deployment/configs/local/run.bat
@@ -1,3 +1,2 @@
-REM new port to avoid conflict with old version running on same system, to make comparison possible.
 @start java -Xmx2g -jar webapp-runner.jar backend --port 5172
-@start java -Xmx2g -jar webapp-runner.jar frontend
+@start java -Xmx2g -jar webapp-runner.jar frontend --port 5173
--- deployment/configs/local/run.sh
+++ deployment/configs/local/run.sh
@@ -3,6 +3,5 @@
 echo "This script does not support Microsoft Windows please use the run.bat script.\n"
 
 trap 'kill %1' SIGINT
-# new port to avoid conflict with old version running on same system, to make comparison possible.
 java -Xmx2g -jar webapp-runner.jar backend --port 5172  &
-java -Xmx2g -jar webapp-runner.jar frontend
+java -Xmx2g -jar webapp-runner.jar frontend --port 5173
